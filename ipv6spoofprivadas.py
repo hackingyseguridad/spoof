f7767fd5b68960a54489406f5eabf59bdd713017
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-04-30 08:17:12 +0200

    Update ipv6spoofprivadas.py

--- ipv6spoofprivadas.py
+++ ipv6spoofprivadas.py
@@ -1,4 +1,4 @@
 import sys
 from scapy.all import *
 
-send((IPv6(src="2a01:1000:1000:1000:1000:1000:1000:1000", dst="2a02:9140:3c00:9200:20a7:3f07:d4a4:4096")/UDP(dport=53),iface="eth1", count=9)
+send((IPv6(src="2001::1", dst="2a02:9140:3c00:9200:20a7:3f07:d4a4:4096")/UDP(dport=53),iface="eth1", count=9)
