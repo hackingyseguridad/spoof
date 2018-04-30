88ee818b0962ab052359c634dc5949b4d9d7d92e
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-04-30 12:31:39 +0200

    Update ipv6spoof.py

--- ipv6spoof.py
+++ ipv6spoof.py
@@ -6,107 +6,109 @@
 import sys
 from scapy.all import *
 
-iporigen = "2a02:9140:3c01:e00:20b:dbff:fe53:37a1"
+ipdestino = "2a02:9140:3c01:1000:7e20:fd5d:dc94:b192"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+iporigen = "2a02:9140:3c01:1100:20b:dbff:fe53:37a1"
+
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "2a02:9140:3c01:e00:20b:dbff:fe53:37a3"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "2001::1"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "::224.0.0.1"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "::192.168.1.1"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "::255.0.0.0"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "0200::1"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "::"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "::1"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "::1.1.1.1"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "0100::"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "0200::"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "2001:db8::"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "2002::"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "2002:0a00::"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "2a02::1"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "2a02:9000::aaaa"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "fc00::"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "fe80::"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "fec0::"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
 
 iporigen = "ff00::"
 
-ipv6 = IPv6(src=iporigen, dst="2a02:9140:3c01:1000:7e20:fd5d:dc94:b192")/ICMPv6EchoRequest()
+ipv6 = IPv6(src=iporigen, dst=ipdestino)/ICMPv6EchoRequest()
 send(ipv6,iface="enp1s12", count=999)
