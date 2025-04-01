437d521260567f160bb363f1e14b83e86fa654aa
hackingyseguridad <antonio_taboada@hotmail.es>
2025-04-01 12:50:10 +0200

    Update spoofnmap.sh

--- spoofnmap.sh
+++ spoofnmap.sh
@@ -14,7 +14,7 @@ cat << "INFO"
 ╬═╬
 ╬═╬
 ╬═╬    spoof V.1.0
-╬═╬    hackingyseguridad.com
+╬═╬    hackingyseguridad.com 2025
 ╬═╬⛑/
 ╬═╬/▌
 ╬═╬/  \
@@ -23,9 +23,12 @@ INFO
 echo
 echo "..."
 echo
+echo "spoofing " $1
+echo
 nmap -Pn -S $1 -p 23 -sV $1
 nmap -Pn -S 127.0.0.1 -p 23 -sV $1
 nmap -Pn -S localhost -p 23 -sV $1
 nmap -Pn --open -S 127.0.0.1  -sACV  -T4 -f -O --max-retries 1 --defeat-rst-ratelimit --script firewall-bypass,http-waf-fingerprint,http-waf-detect $1 -p 23
+nmap -Pn --open -S $1 -sACV  -T4 -f -O --max-retries 1 --defeat-rst-ratelimit --script firewall-bypass,http-waf-fingerprint,http-waf-detect $1 -p 23
 
 
