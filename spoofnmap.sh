162581c7f40a830c2ed22a2f1f0606580a8a9772
hackingyseguridad <antonio_taboada@hotmail.es>
2025-04-01 12:37:56 +0200

    Update spoofnmap.sh

--- spoofnmap.sh
+++ spoofnmap.sh
@@ -1,7 +1,28 @@
+#!/bin/bash
+# (c) hackingyseguridad.com 2025
 
+cat << "INFO"
 
+▬▬▬.◙.▬▬▬
+═▂▄▄▓▄▄▂
+◢◤ █▀▀████▄▄▄▄◢◤
+█▄ █ █▄ ███▀▀▀▀▀▀▀╬
+◥█████◤
+══╩══╩═
+╬═╬
+╬═╬
+╬═╬
+╬═╬
+╬═╬    spoof V.1.0
+╬═╬    hackingyseguridad.com
+╬═╬⛑/
+╬═╬/▌
+╬═╬/  \
 
-
-
-nmap -S $1 -p 23 -sV $1
-nmap -S 127.0.0.1 -p 23 -sV $1
+INFO
+echo
+echo "..."
+echo
+nmap -Pn -S $1 -p 23 -sV $1
+nmap -Pn -S 127.0.0.1 -p 23 -sV $1
+nmap -Pn -S localhost -p 23 -sV $1
