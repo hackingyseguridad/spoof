6a3d005ccb877c7173a63b04178ab2c42dd26c0b
hackingyseguridad <antonio_taboada@hotmail.es>
2025-04-01 12:46:59 +0200

    Update spoofnmap.sh

--- spoofnmap.sh
+++ spoofnmap.sh
@@ -26,5 +26,6 @@ echo
 nmap -Pn -S $1 -p 23 -sV $1
 nmap -Pn -S 127.0.0.1 -p 23 -sV $1
 nmap -Pn -S localhost -p 23 -sV $1
+nmap -Pn --open -S 127.0.0.1  -sACV  -T4 -f -O --max-retries 1 --defeat-rst-ratelimit --script firewall-bypass,http-waf-fingerprint,http-waf-detect $1 -p 23
 
 
